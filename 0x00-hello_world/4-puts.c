4eb2d5e95a9e18b33c2a5e8cb77233f3efa14c15
Jude Nwanna <judeonnet13@gmail.com>
2023-10-05 19:20:47 -0700

    TASK: 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C programe that prints a line function puts
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
