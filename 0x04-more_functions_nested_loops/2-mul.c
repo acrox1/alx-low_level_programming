7fc3c483fa5321775c9948bb84fe5136bf529340
Jude Nwanna <judeonnet13@gmail.com>
2023-10-12 21:37:19 -0700

    TASK: 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - a function that checks for lowercase character.
+ * @a: is first parameter
+ * @b: is second parameter
+ * Return: 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
