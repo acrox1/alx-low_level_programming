b5e1ed56a3e969d3ecee21310183b8a90feccf81
Jude Nwanna <judeonnet13@gmail.com>
2023-10-17 18:37:33 -0700

    TASK: 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str: string argument
+ * Return: void (success)
+ */
+void _puts(char *str)
+{
+
+	puts(str);
+}
