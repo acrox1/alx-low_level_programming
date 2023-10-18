fccd989cf12a0d9767eebccfb8fc810f5ff23c7c
Jude Nwanna <judeonnet13@gmail.com>
2023-10-17 18:25:51 -0700

    TASK:2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: string argument
+ * Return: 0 (success)
+ */
+int _strlen(char *s)
+{
+	int _slen;
+
+	_slen = strlen(s);
+
+	return (_slen);
+}
