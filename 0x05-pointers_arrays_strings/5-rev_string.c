78cc756ecbf4c7352b7992976bf0fd2e2a3eee3e
Jude Nwanna <judeonnet13@gmail.com>
2023-10-21 23:59:50 -0700

    Task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - a function that reverses a string.
+ * @s: string argument
+ * Return: void (success)
+ */
+void rev_string(char *s)
+{
+	int length = strlen(s);
+
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+		/*_putchar(s[i]);*/
+	}
+	/*_putchar('\n');*/
+	printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define HEADER_FILE
 
 #include <stdio.h>
-int _putchar(char c);
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,7 +13,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _putchar(char c);
 
 
 
