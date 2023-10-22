312ec9ad4b9f993d9191cbd650266f827f8cc08f
Jude Nwanna <judeonnet13@gmail.com>
2023-10-22 02:58:24 -0700

    Corrected: TASK 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 
 	/*a[n];*/
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
-		if (i != n - 2)
+		if (i != n - 1)
 		{
 			printf("%d, ", a[i]);
 		}
