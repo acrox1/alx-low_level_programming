3f78d58396ed524c33ef9d965b4dfe8db022c3cb
Jude Nwanna <judeonnet13@gmail.com>
2023-10-25 10:51:50 -0700

    TASK:0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: pointer to a string
+ * Return: void (success)
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
