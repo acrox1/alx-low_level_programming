1b1315a4eda4846d480d1998971f980953df503a
Jude Nwanna <judeonnet13@gmail.com>
2023-10-30 13:30:27 -0700

    TASK: 4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int res;
+
+	res = 0;
+
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+	}
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) != 0)
+			{
+				res += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	printf("%d\n", res);
+	return (0);
+}
